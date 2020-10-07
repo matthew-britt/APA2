7cdd41cb56e21afe09a611a4bbd5b558fc57701b
github username <mwbritt81@gmail.com>
2020-10-07 10:09:39 -0500

    added techlist.js

---
+++ techlist.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+    // let params = new URLSearchParams(location.search);
+    // let newParams = params.toString().replace("=", "").replace(/\+/g, " ");
+  
+    // fetchInfo(newParams);
+
+    'Suck it, Trebek'
+  });
