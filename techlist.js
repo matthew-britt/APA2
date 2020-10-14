9358f36df879a67e779159b71b1e8db29db098ee
github username <mwbritt81@gmail.com>
2020-10-13 19:18:07 -0500

    uniqueTagsArray

--- techlist.js
+++ techlist.js
@@ -7,9 +7,12 @@ document.addEventListener("DOMContentLoaded", () => {
   console.log("Suck it, Trebek");
 });
 
+function onlyUnique(value, index, self) {
+  return self.indexOf(value) === index;
+}
+
 function fetchTechList() {
   let tagsArray = [];
-  let updatedTags = [];
   fetch("https://api.airtable.com/v0/appT5nNiLF8Dr1wwj/Technology%20List/", {
     headers: {
       Authorization: "Bearer keyemv7utChwq4g5e",
@@ -20,18 +23,18 @@ function fetchTechList() {
       for (let i in json.records) {
         tagsArray.push(json.records[i]["fields"]["Tags"]);
       }
-      
-      for (let m = 0; m < tagsArray.length; m++) {
-          for (let n = 0; n < tagsArray[m].length; n++) {
-             // updatedTags.push(tagsArray[m][n]);
-              console.log(tagsArray[m])
-          }
-      }
+
+      let allTagsArray = tagsArray.toString().split(",");
+
+      let uniqueTagsArray = allTagsArray.filter(onlyUnique)
+
+      console.log(uniqueTagsArray);
+
+      //   for (let m = 0; m < tagsArray.length; m++) {
+      //       for (let n = 0; n < tagsArray[m].length; n++) {
+      //          // updatedTags.push(tagsArray[m][n]);
+      //       }
+      //   }
     });
-//console.log(tagsArray[0])
-//   for (let i = 0; i < tagsArray.length; i++) {
-//     for (let j = 0; j < i.length; j++) {
-//     }
-//   }
-};
+}
 //https://api.airtable.com/v0/appT5nNiLF8Dr1wwj/Technology%20List?maxRecords=3&view=Grid%20view
