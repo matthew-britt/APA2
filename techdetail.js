b4fb318cae242646d2f239dfda32b73919955f04
github username <mwbritt81@gmail.com>
2020-10-21 16:58:35 -0500

    changed href on toolNameLi

--- techdetail.js
+++ techdetail.js
@@ -1,8 +1,44 @@
 document.addEventListener("DOMContentLoaded", () => {
-    // let params = new URLSearchParams(location.search);
-    // let newParams = params.toString().replace("=", "").replace(/\+/g, " ");
-  
-    // fetchInfo(newParams);
+  // let params = new URLSearchParams(location.search);
+  // let newParams = params.toString().replace("=", "").replace(/\+/g, " ");
 
-    console.log('Suck it, Trebek')
-  });
+  // fetchInfo(newParams);
+
+  console.log("Suck it, Trebek");
+});
+
+function el(id) {
+  return document.getElementById(id);
+}
+
+// function showTechDetail(e) {
+//   e.preventDefault();
+//   let tool = e.target.id;
+//   fetch("https://api.airtable.com/v0/appT5nNiLF8Dr1wwj/Technology%20List/", {
+//     headers: {
+//       Authorization: "Bearer keyemv7utChwq4g5e",
+//     },
+//   })
+//     .then((res) => res.json())
+//     .then((json) => {
+//       for (let i in json.records) {
+//         if (json.records[i]["fields"]["Name"] === tool) {
+//           let website = json.records[i]["fields"]["Website"];
+//           let tags = json.records[i]["fields"]["Tags"].toString();
+//           let description = json.records[i]["fields"]["Description"];
+//           let awio = json.records[i]["fields"]["Animal Welfare Industry Only"];
+//           let contact = json.records[i]["fields"]["Contact"];
+//           let contactEmail = json.records[i]["fields"]["Contact Email"];
+//           let pricingModel = json.records[i]["fields"]["Pricing Model"];
+//           let pricingDetails = json.records[i]["fields"]["Pricing Details"];
+//           let attachments = json.records[i]["fields"]["Attachments"];
+//           let caseStudies = json.records[i]["fields"]["Case Studies"];
+//           //console.log(caseStudies)
+
+//           let toolLi = document.createElement("li");
+//           toolLi.innerText = tool;
+//           el("tool").appendChild(toolLi);
+//         }
+//       }
+//     });
+// }
--- techlist.js
+++ techlist.js
@@ -1,3 +1,5 @@
+//import { showTechDetail } from "techdetail.js";
+
 document.addEventListener("DOMContentLoaded", () => {
   // let params = new URLSearchParams(location.search);
   // let newParams = params.toString().replace("=", "").replace(/\+/g, " ");
@@ -8,7 +10,7 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 function el(id) {
-    return document.getElementById(id);
+  return document.getElementById(id);
 }
 
 function onlyUnique(value, index, self) {
@@ -20,7 +22,7 @@ function renderEachTag(tag) {
   tagUl.setAttribute("class", "list-group");
   tagUl.setAttribute("id", tag);
   tagUl.innerText = tag;
-  document.getElementById("tagId").append(tagUl);
+  document.getElementById("tagId").appendChild(tagUl);
 }
 
 function fetchTechList() {
@@ -49,13 +51,14 @@ function fetchTechList() {
         for (let i in json.records) {
           if (json.records[i]["fields"]["Tags"].includes(tag)) {
             let toolName = json.records[i]["fields"]["Name"];
-            const toolLi = document.createElement("a");
-            toolLi.setAttribute("class", "list-group-item");
-            toolLi.setAttribute("id", toolName);
-            toolLi.innerText = toolName;
-            toolLi.href = toolName;
-            toolLi.addEventListener('click', showTechDetail);
-            document.getElementById(tag).append(toolLi);
+            const toolNameLi = document.createElement("a");
+            toolNameLi.setAttribute("class", "list-group-item");
+            toolNameLi.setAttribute("id", toolName);
+            toolNameLi.innerText = toolName;
+            toolNameLi.href = "https://matthew-britt.github.io/APA2/techdetail.html";
+            toolNameLi.target = "_blank";
+            toolNameLi.addEventListener("click", showTechDetail);
+            document.getElementById(tag).append(toolNameLi);
           }
         }
       }
@@ -63,9 +66,9 @@ function fetchTechList() {
 }
 
 function showTechDetail(e) {
-    e.preventDefault();
-    let tool = e.target.id;
-    fetch("https://api.airtable.com/v0/appT5nNiLF8Dr1wwj/Technology%20List/", {
+  e.preventDefault();
+  let tool = e.target.id;
+  fetch("https://api.airtable.com/v0/appT5nNiLF8Dr1wwj/Technology%20List/", {
     headers: {
       Authorization: "Bearer keyemv7utChwq4g5e",
     },
@@ -73,21 +76,23 @@ function showTechDetail(e) {
     .then((res) => res.json())
     .then((json) => {
       for (let i in json.records) {
-          if (json.records[i]["fields"]["Name"] === tool) {
-              let website = json.records[i]["fields"]["Website"];
-              let tags = json.records[i]["fields"]["Tags"].toString();
-              let description = json.records[i]["fields"]["Description"];
-              let awio = json.records[i]["fields"]["Animal Welfare Industry Only"];
-              let contact = json.records[i]["fields"]["Contact"];
-              let contactEmail = json.records[i]["fields"]["Contact Email"];
-              let pricingModel = json.records[i]["fields"]["Pricing Model"];
-              let pricingDetails = json.records[i]["fields"]["Pricing Details"];
-              let attachments = json.records[i]["fields"]["Attachments"];
-              let caseStudies = json.records[i]["fields"]["Case Studies"];
-              console.log(caseStudies)
-            }
+        if (json.records[i]["fields"]["Name"] === tool) {
+          let website = json.records[i]["fields"]["Website"];
+          let tags = json.records[i]["fields"]["Tags"].toString();
+          let description = json.records[i]["fields"]["Description"];
+          let awio = json.records[i]["fields"]["Animal Welfare Industry Only"];
+          let contact = json.records[i]["fields"]["Contact"];
+          let contactEmail = json.records[i]["fields"]["Contact Email"];
+          let pricingModel = json.records[i]["fields"]["Pricing Model"];
+          let pricingDetails = json.records[i]["fields"]["Pricing Details"];
+          let attachments = json.records[i]["fields"]["Attachments"];
+          let caseStudies = json.records[i]["fields"]["Case Studies"];
+          //console.log(caseStudies)
+
+          let toolLi = document.createElement("li");
+          toolLi.innerText = tool;
+          el("tool").appendChild(toolLi);
+        }
       }
-
-
     });
 }
