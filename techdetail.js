97870ea74c0f170558a68d5a736f8af44ae0cbe4
github username <mwbritt81@gmail.com>
2020-10-22 12:03:37 -0500

    params of techdetail

--- techdetail.js
+++ techdetail.js
@@ -1,9 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // let params = new URLSearchParams(location.search);
-  // let newParams = params.toString().replace("=", "").replace(/\+/g, " ");
-
+  
   // fetchInfo(newParams);
-
+  
+  let params = location.search;
+  let newParams = params.toString().replace("?", "").replace(/\%20/g, " ");
+  console.log(newParams)
   console.log("Suck it, Trebek");
 });
 
@@ -11,34 +12,35 @@ function el(id) {
   return document.getElementById(id);
 }
 
-// function showTechDetail(e) {
-//   e.preventDefault();
-//   let tool = e.target.id;
-//   fetch("https://api.airtable.com/v0/appT5nNiLF8Dr1wwj/Technology%20List/", {
-//     headers: {
-//       Authorization: "Bearer keyemv7utChwq4g5e",
-//     },
-//   })
-//     .then((res) => res.json())
-//     .then((json) => {
-//       for (let i in json.records) {
-//         if (json.records[i]["fields"]["Name"] === tool) {
-//           let website = json.records[i]["fields"]["Website"];
-//           let tags = json.records[i]["fields"]["Tags"].toString();
-//           let description = json.records[i]["fields"]["Description"];
-//           let awio = json.records[i]["fields"]["Animal Welfare Industry Only"];
-//           let contact = json.records[i]["fields"]["Contact"];
-//           let contactEmail = json.records[i]["fields"]["Contact Email"];
-//           let pricingModel = json.records[i]["fields"]["Pricing Model"];
-//           let pricingDetails = json.records[i]["fields"]["Pricing Details"];
-//           let attachments = json.records[i]["fields"]["Attachments"];
-//           let caseStudies = json.records[i]["fields"]["Case Studies"];
-//           //console.log(caseStudies)
+//let tool = e.target.id;
 
-//           let toolLi = document.createElement("li");
-//           toolLi.innerText = tool;
-//           el("tool").appendChild(toolLi);
-//         }
-//       }
-//     });
-// }
+function showTechDetail(tool) {
+  //e.preventDefault();
+  fetch("https://api.airtable.com/v0/appT5nNiLF8Dr1wwj/Technology%20List/", {
+    headers: {
+      Authorization: "Bearer keyemv7utChwq4g5e",
+    },
+  })
+    .then((res) => res.json())
+    .then((json) => {
+      for (let i in json.records) {
+        if (json.records[i]["fields"]["Name"] === tool) {
+          let website = json.records[i]["fields"]["Website"];
+          let tags = json.records[i]["fields"]["Tags"].toString();
+          let description = json.records[i]["fields"]["Description"];
+          let awio = json.records[i]["fields"]["Animal Welfare Industry Only"];
+          let contact = json.records[i]["fields"]["Contact"];
+          let contactEmail = json.records[i]["fields"]["Contact Email"];
+          let pricingModel = json.records[i]["fields"]["Pricing Model"];
+          let pricingDetails = json.records[i]["fields"]["Pricing Details"];
+          let attachments = json.records[i]["fields"]["Attachments"];
+          let caseStudies = json.records[i]["fields"]["Case Studies"];
+          //console.log(caseStudies)
+
+          let toolLi = document.createElement("li");
+          toolLi.innerText = tool;
+          el("tool").appendChild(toolLi);
+        }
+      }
+    });
+}
--- techlist.js
+++ techlist.js
@@ -55,7 +55,7 @@ function fetchTechList() {
             toolNameLi.setAttribute("class", "list-group-item");
             toolNameLi.setAttribute("id", toolName);
             toolNameLi.innerText = toolName;
-            toolNameLi.href = `./techdetail.html/${toolName}`;
+            toolNameLi.href = `./techdetail.html?${toolName}`;
             toolNameLi.target = "_blank";
             //toolNameLi.addEventListener("click", showTechDetail);
             document.getElementById(tag).append(toolNameLi);
@@ -65,34 +65,34 @@ function fetchTechList() {
     });
 }
 
-function showTechDetail(e) {
-  e.preventDefault();
-  let tool = e.target.id;
-  fetch("https://api.airtable.com/v0/appT5nNiLF8Dr1wwj/Technology%20List/", {
-    headers: {
-      Authorization: "Bearer keyemv7utChwq4g5e",
-    },
-  })
-    .then((res) => res.json())
-    .then((json) => {
-      for (let i in json.records) {
-        if (json.records[i]["fields"]["Name"] === tool) {
-          let website = json.records[i]["fields"]["Website"];
-          let tags = json.records[i]["fields"]["Tags"].toString();
-          let description = json.records[i]["fields"]["Description"];
-          let awio = json.records[i]["fields"]["Animal Welfare Industry Only"];
-          let contact = json.records[i]["fields"]["Contact"];
-          let contactEmail = json.records[i]["fields"]["Contact Email"];
-          let pricingModel = json.records[i]["fields"]["Pricing Model"];
-          let pricingDetails = json.records[i]["fields"]["Pricing Details"];
-          let attachments = json.records[i]["fields"]["Attachments"];
-          let caseStudies = json.records[i]["fields"]["Case Studies"];
-          //console.log(caseStudies)
+// function showTechDetail(e) {
+//   e.preventDefault();
+//   let tool = e.target.id;
+//   fetch("https://api.airtable.com/v0/appT5nNiLF8Dr1wwj/Technology%20List/", {
+//     headers: {
+//       Authorization: "Bearer keyemv7utChwq4g5e",
+//     },
+//   })
+//     .then((res) => res.json())
+//     .then((json) => {
+//       for (let i in json.records) {
+//         if (json.records[i]["fields"]["Name"] === tool) {
+//           let website = json.records[i]["fields"]["Website"];
+//           let tags = json.records[i]["fields"]["Tags"].toString();
+//           let description = json.records[i]["fields"]["Description"];
+//           let awio = json.records[i]["fields"]["Animal Welfare Industry Only"];
+//           let contact = json.records[i]["fields"]["Contact"];
+//           let contactEmail = json.records[i]["fields"]["Contact Email"];
+//           let pricingModel = json.records[i]["fields"]["Pricing Model"];
+//           let pricingDetails = json.records[i]["fields"]["Pricing Details"];
+//           let attachments = json.records[i]["fields"]["Attachments"];
+//           let caseStudies = json.records[i]["fields"]["Case Studies"];
+//           //console.log(caseStudies)
 
-          let toolLi = document.createElement("li");
-          toolLi.innerText = tool;
-          el("tool").appendChild(toolLi);
-        }
-      }
-    });
-}
+//           let toolLi = document.createElement("li");
+//           toolLi.innerText = tool;
+//           el("tool").appendChild(toolLi);
+//         }
+//       }
+//     });
+// }
