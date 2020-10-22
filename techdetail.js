3e4bb01b86be75077bc66370e7e7936168c9d682
github username <mwbritt81@gmail.com>
2020-10-22 12:32:55 -0500

    rendered websiteLi

--- techdetail.js
+++ techdetail.js
@@ -1,10 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
   
-  // fetchInfo(newParams);
-  
   let params = location.search;
   let newParams = params.toString().replace("?", "").replace(/\%20/g, " ");
-  console.log(newParams)
+  renderTechDetail(newParams)
   console.log("Suck it, Trebek");
 });
 
@@ -12,10 +10,7 @@ function el(id) {
   return document.getElementById(id);
 }
 
-//let tool = e.target.id;
-
-function showTechDetail(tool) {
-  //e.preventDefault();
+function renderTechDetail(tool) {
   fetch("https://api.airtable.com/v0/appT5nNiLF8Dr1wwj/Technology%20List/", {
     headers: {
       Authorization: "Bearer keyemv7utChwq4g5e",
@@ -26,7 +21,7 @@ function showTechDetail(tool) {
       for (let i in json.records) {
         if (json.records[i]["fields"]["Name"] === tool) {
           let website = json.records[i]["fields"]["Website"];
-          let tags = json.records[i]["fields"]["Tags"].toString();
+          let tags = json.records[i]["fields"]["Tags"];
           let description = json.records[i]["fields"]["Description"];
           let awio = json.records[i]["fields"]["Animal Welfare Industry Only"];
           let contact = json.records[i]["fields"]["Contact"];
@@ -35,11 +30,18 @@ function showTechDetail(tool) {
           let pricingDetails = json.records[i]["fields"]["Pricing Details"];
           let attachments = json.records[i]["fields"]["Attachments"];
           let caseStudies = json.records[i]["fields"]["Case Studies"];
-          //console.log(caseStudies)
+          console.log(tags[0])
 
           let toolLi = document.createElement("li");
           toolLi.innerText = tool;
           el("tool").appendChild(toolLi);
+
+          let websiteLi = document.createElement('a');
+          websiteLi.innerText = website;
+          websiteLi.title = 'Click here';
+          websiteLi.href = website;
+          websiteLi.target = "_blank";
+          el("website").appendChild(websiteLi);
         }
       }
     });
