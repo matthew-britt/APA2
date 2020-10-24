f1f5f523753b811f4ce9094fa8348814baa03404
github username <mwbritt81@gmail.com>
2020-10-24 10:45:04 -0500

    rendered case study header

--- techcasestudy.js
+++ techcasestudy.js
@@ -1,8 +1,38 @@
 document.addEventListener("DOMContentLoaded", () => {
-    // let params = new URLSearchParams(location.search);
-    // let newParams = params.toString().replace("=", "").replace(/\+/g, " ");
-  
-    // fetchInfo(newParams);
+  // let params = new URLSearchParams(location.search);
+  // let newParams = params.toString().replace("=", "").replace(/\+/g, " ");
+  let params = location.search.replace("?", "");
+  renderCaseStudyPage(params);
 
-    console.log('Suck it again, Trebek')
-  });
+  console.log("Suck it again, Trebek");
+});
+
+function el(id) {
+  return document.getElementById(id);
+}
+
+function renderCaseStudyPage(study) {
+  fetch(
+    `https://api.airtable.com/v0/appT5nNiLF8Dr1wwj/Case%20Studies/${study}`,
+    {
+      headers: {
+        Authorization: "Bearer keyemv7utChwq4g5e",
+      },
+    }
+  )
+    .then((res) => res.json())
+    .then((json) => {
+      console.log(json['fields'])
+      let studyTitle = json["fields"]["Title"];
+      let caseStudiesLi = document.createElement("li");
+      caseStudiesLi.setAttribute("id", studyTitle);
+      //el("caseStudies").appendChild(caseStudiesLi);
+
+      let caseStudiesA = document.createElement("a");
+      caseStudiesA.innerText = studyTitle;
+      caseStudiesA.title = "Click here";
+      caseStudiesA.href = `./techcasestudy.html?${study}`;
+      caseStudiesA.target = "_blank";
+      //el(studyTitle).appendChild(caseStudiesA);
+    });
+}
